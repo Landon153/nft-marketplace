1236092e89ce50462f2a0e63c28279f0cf94b58f
Nguyễn Thành Đạt - Landon <43365714+Landon153@users.noreply.github.com>
2025-09-29 10:20:32 +0000

    feat: add SQLModel struct with masking and initialization functions

---
+++ nft-marketplace-backend-golang/common/sql_model.go
@@ -0,0 +1,28 @@
+package common
+
+import (
+	"time"
+)
+
+type SQLModel struct {
+	Id        int        `json:"-" gorm:"column:id;"`
+	FakeId    *UID       `json:"id" gorm:"-"`
+	Status    string     `json:"status" gorm:"column:status;"`
+	CreatedAt *time.Time `json:"created_at" gorm:"column:created_at;"`
+	UpdatedAt *time.Time `json:"updated_at" gorm:"column:updated_at;"`
+}
+
+func (m *SQLModel) Mask(dbType int) {
+	uid := NewUID(uint32(m.Id), dbType, 1)
+	m.FakeId = &uid
+}
+
+func NewSQLModel() SQLModel {
+	now := time.Now().UTC()
+
+	return SQLModel{
+		Status:    "activated",
+		CreatedAt: &now,
+		UpdatedAt: &now,
+	}
+}
